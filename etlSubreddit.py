b4ac17deef892638407b1b71b388517cda4203eb
Henry Tom <henry.tom.sf@gmail.com>
2020-08-08 23:36:09 -0700

    get submissions example

---
+++ etlSubreddit.py
@@ -0,0 +1,29 @@
+from getCredentials import AppOAuth
+
+
+def getSubmissionEvents(reddit,
+                        subreddit,
+                        limit=256):
+    subredditDataList = []
+    for submission in reddit.subreddit(subreddit).top(limit=limit):
+        subredditDataList.append(submission)
+
+    return subredditDataList
+
+def getSubmissionProperties(eventList, property='upvote_ratio'):
+    propertyList = []
+    for submission in eventList:
+        propertyList.append(submission.upvote_ratio)
+
+    return propertyList
+
+
+if __name__ == '__main__':
+    aoa = AppOAuth()
+
+    # test 1
+    eventList = getSubmissionEvents(aoa.reddit, 'popular', limit=10)
+    upvoteRatioList = getSubmissionProperties(eventList)
+
+    print('Upvote ratios: ', upvoteRatioList)
+    # [0.98, 0.97, 0.97, 0.96, 0.97, 0.97, 0.98, 0.96, 0.97, 0.96]
